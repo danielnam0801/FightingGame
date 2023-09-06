223f893b917414957c0e59bf5bbd6b8244446318
minhyeok8278 <adblack1234@naver.com>
2023-09-07 00:25:03 +0900

    make camera shaking(hitted)

---
+++ Assets/01_Scripts/CameraShake.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using System;
+
+public class CameraShake : MonoBehaviour
+{
+    private CinemachineVirtualCamera vCam;
+    private CinemachineBasicMultiChannelPerlin perlin;
+
+   [SerializeField]  float hitAmplitudeGain = 2, hitFrequencyGain = 2, shakeTime = 1;
+    bool isShaking = false;
+    float shakeTimeElapsed = 0f;
+
+    private void Awake()
+    {
+        vCam = GetComponent<CinemachineVirtualCamera>();
+        perlin = vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    void Start()
+    {
+
+        //if(Input.GetKeyDown(KeyCode.W))
+        //{
+        //    StartCoroutine(StopShake());
+        //}
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(ShakeCamera());
+        }
+    }
+    IEnumerator ShakeCamera()
+    {
+        perlin.m_AmplitudeGain = hitAmplitudeGain;
+        perlin.m_FrequencyGain = hitFrequencyGain;
+        yield return new WaitForSeconds(0.1f);
+        StartCoroutine(StopShake());
+    }
+    IEnumerator StopShake()
+    {
+        perlin.m_AmplitudeGain = 0;
+        perlin.m_FrequencyGain = 0;
+        yield return new WaitForSeconds(0.5f);
+    }
+}
