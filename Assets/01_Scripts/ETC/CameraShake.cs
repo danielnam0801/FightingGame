45967731f75603a71006880556c430776dbc2302
minhyeok8278 <adblack1234@naver.com>
2023-10-05 00:58:39 +0900

    timeline track group

--- Assets/01_Scripts/ETC/CameraShake.cs
+++ Assets/01_Scripts/ETC/CameraShake.cs
@@ -11,13 +11,15 @@ public class CameraShake : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera vCam;
     [SerializeField] CinemachineVirtualCamera vCam2;
+
     private CinemachineBasicMultiChannelPerlin perlin;
-    [SerializeField] CinemachineFreeLook blueGuyFCam;
-    [SerializeField] CinemachineFreeLook redGuyFCam;
-    //[SerializeField] VisualEffect hitEffect;
+
+    [SerializeField] CinemachineFreeLook PlayerFCam;
+    [SerializeField] CinemachineFreeLook AIFCam;
+
     [SerializeField] PlayableDirector introDirector;
-    [SerializeField] PlayableDirector winningDirector;
-    [SerializeField] PlayableDirector losingDirector;
+    [SerializeField] TrackAsset track;
+
 
     [SerializeField] float hitAmplitudeGain = 2, hitFrequencyGain = 2, shakeTime = 1;
     Vector3 vec = new Vector3(0.17f, -0.2f, 3.1f);
@@ -26,35 +28,30 @@ public class CameraShake : MonoBehaviour
 
     private void Awake()
     {
-
+        track.muted = true;
         vCam = GameObject.Find("IntroCamera").GetComponent<CinemachineVirtualCamera>();
         vCam2 = GameObject.Find("ShakeCamera").GetComponent<CinemachineVirtualCamera>();
         perlin = vCam2.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
 
-        blueGuyFCam = GameObject.Find("BlueGuyFreeLook").GetComponent<CinemachineFreeLook>();
-        redGuyFCam = GameObject.Find("RedGuyFreeLook").GetComponent<CinemachineFreeLook>();
+        PlayerFCam = GameObject.Find("PlayerFreeLook").GetComponent<CinemachineFreeLook>();
+        AIFCam = GameObject.Find("AIFreeLook").GetComponent<CinemachineFreeLook>();
 
 
-        blueGuyFCam.m_XAxis.m_InputAxisName = ""; // X축 입력 무시
-        blueGuyFCam.m_YAxis.m_InputAxisName = ""; // Y축 입력 무시
+        PlayerFCam.m_XAxis.m_InputAxisName = ""; // X축 입력 무시
+        PlayerFCam.m_YAxis.m_InputAxisName = ""; // Y축 입력 무시
 
-        redGuyFCam.m_XAxis.m_InputAxisName = "";
-        redGuyFCam.m_YAxis.m_InputAxisName = "";
+        AIFCam.m_XAxis.m_InputAxisName = "";
+        AIFCam.m_YAxis.m_InputAxisName = "";
 
-        winningDirector = GameObject.Find("PlayerUnit").GetComponent<PlayableDirector>();
-        losingDirector = GameObject.Find("AIUnit").GetComponent<PlayableDirector>();
         introDirector = GameObject.Find("IntroTimeLine").GetComponent<PlayableDirector>();
 
-        blueGuyFCam.gameObject.SetActive(false);
-        redGuyFCam.gameObject.SetActive(false);
+        PlayerFCam.gameObject.SetActive(false);
+        AIFCam.gameObject.SetActive(false);
     }
 
     void Start()
     {
         introDirector.Play();
-        //winningDirector.Stop();
-        //losingDirector.Stop();
-
     }
     private void Update()
     {
@@ -88,7 +85,6 @@ public class CameraShake : MonoBehaviour
         perlin.m_FrequencyGain = hitFrequencyGain;
         yield return new WaitForSeconds(0.1f);
         StartCoroutine(StopShake());
-        //hitEffect.Play();
     }
     IEnumerator StopShake()
     {
@@ -98,16 +94,14 @@ public class CameraShake : MonoBehaviour
     }
     IEnumerator WinnerRotateCamera()
     {
-        blueGuyFCam.gameObject.SetActive(true);
-        introDirector.Stop();
-        winningDirector.Play();
+        PlayerFCam.gameObject.SetActive(true);
+
         yield return null;
     }
     IEnumerator LoserRotateCamera()
     {
-        redGuyFCam.gameObject.SetActive(true);
+        AIFCam.gameObject.SetActive(true);
         introDirector.Stop();
-        losingDirector.Play();
         yield return null;
     }
 }
