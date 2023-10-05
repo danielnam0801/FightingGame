5b4df50e57f9671df144e10d5888657494117c7d
minhyeok8278 <adblack1234@naver.com>
2023-10-06 00:37:43 +0900

    haenem

--- Assets/01_Scripts/ETC/CameraShake.cs
+++ Assets/01_Scripts/ETC/CameraShake.cs
@@ -18,17 +18,40 @@ public class CameraShake : MonoBehaviour
     [SerializeField] CinemachineFreeLook AIFCam;
 
     [SerializeField] PlayableDirector introDirector;
-    [SerializeField] TrackAsset track;
+    [SerializeField] PlayableDirector winDirector;
+
+
+
+    private TrackAsset winTrack = null;
+    private TrackAsset winCameraTrack = null;
+    private TrackAsset loseTrack = null;
+    private TrackAsset loseCameraTrack = null;
+
+    private TimelineAsset winTimeline;
+    private TimelineAsset loseTimeline;
+
+    string winTrackName = "Winning";
+    string loseTrackName = "Losing";
+    string winCameraTrackName = "WinCameraSpin";
+    string loseCameraTrackName = "LoseCameraSpin";
+
 
 
     [SerializeField] float hitAmplitudeGain = 2, hitFrequencyGain = 2, shakeTime = 1;
     Vector3 vec = new Vector3(0.17f, -0.2f, 3.1f);
-    private bool isWin = false;
-    private bool isLose = false;
 
     private void Awake()
     {
-        track.muted = true;
+        winTimeline = winDirector.playableAsset as TimelineAsset;
+        loseTimeline = winDirector.playableAsset as TimelineAsset;
+
+
+
+        winTrack = winTimeline.GetOutputTrack(0);
+        winCameraTrack = winTimeline.GetOutputTrack(0);
+        loseTrack = loseTimeline.GetOutputTrack(0);
+        loseCameraTrack = loseTimeline.GetOutputTrack(0);
+
         vCam = GameObject.Find("IntroCamera").GetComponent<CinemachineVirtualCamera>();
         vCam2 = GameObject.Find("ShakeCamera").GetComponent<CinemachineVirtualCamera>();
         perlin = vCam2.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
@@ -36,6 +59,7 @@ public class CameraShake : MonoBehaviour
         PlayerFCam = GameObject.Find("PlayerFreeLook").GetComponent<CinemachineFreeLook>();
         AIFCam = GameObject.Find("AIFreeLook").GetComponent<CinemachineFreeLook>();
 
+        
 
         PlayerFCam.m_XAxis.m_InputAxisName = ""; // X축 입력 무시
         PlayerFCam.m_YAxis.m_InputAxisName = ""; // Y축 입력 무시
@@ -72,6 +96,7 @@ public class CameraShake : MonoBehaviour
         }
         if(Input.GetKeyDown(KeyCode.R))
         {
+            introDirector.Stop();
             StartCoroutine(LoserRotateCamera());
             vCam2.gameObject.SetActive(false);
         }
@@ -95,13 +120,37 @@ public class CameraShake : MonoBehaviour
     IEnumerator WinnerRotateCamera()
     {
         PlayerFCam.gameObject.SetActive(true);
+        introDirector.Stop();
+        foreach (TrackAsset track in winTimeline.GetOutputTracks())
+        {
+            if (track.name == loseTrackName)
+            {
+                track.locked = true;
+                track.muted = true;
+                loseTrack = track;
+                loseCameraTrack = track;
+            }
+        }
+        winDirector.Play();
 
         yield return null;
     }
     IEnumerator LoserRotateCamera()
     {
-        AIFCam.gameObject.SetActive(true);
+        PlayerFCam.gameObject.SetActive(true);
         introDirector.Stop();
+        foreach(TrackAsset track in loseTimeline.GetOutputTracks())
+        {
+            if(track.name == winTrackName)
+            {
+                track.locked = true;
+                track.muted = true;
+                winTrack = track;
+                winCameraTrack = track;
+            }
+        }
+        winDirector.Play();
+
         yield return null;
     }
 }
